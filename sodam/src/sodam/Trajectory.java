219f240216fac4f57022c98680ae543ce6edb6fb
James Mills <MillsJ1438@sduhsd.lan>
2019-05-29 09:15:55 -0700

    trajectory, just a straight line

---
+++ sodam/src/sodam/Trajectory.java
@@ -0,0 +1,12 @@
+package sodam;
+
+public class Trajectory {
+	private double xO; //initial position
+	private double vO; //initial velocity = to chargebar
+	private double time; //time in seconds
+	private double g = -9.81; //gravitational accel
+	
+	private double distance = vO*time;
+	//dart.setPos(cursor.getX() , cursor.getY());
+	
+}	
