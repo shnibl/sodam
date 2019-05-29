95655b51e4409c504dbe7f79b76dd3c5b864467e
dan00b781 <51128175+dan00b781@users.noreply.github.com>
2019-05-29 08:39:13 -0700

    added keyEvent and keyListener classes

---
+++ sodam/src/sodam/KeyboardInput.java
@@ -0,0 +1,34 @@
+package sodam;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInput implements KeyListener {
+	private static final int KEY_COUNT = 256;
+	
+	private enum KeyState {
+		RELEASED,
+		PRESSED,
+		ONCE
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
--- sodam/src/sodam/Main.java
+++
@@ -1,7 +0,0 @@
-package sodam;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("ZA WARUDO!");
-	}
-}
