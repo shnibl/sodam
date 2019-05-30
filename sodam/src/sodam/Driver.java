def51d7d8d6eac330763845e4d53a99e270bf310
Shnibl <cadester5@gmail.com>
2019-05-30 09:19:02 -0700

    stuff

--- sodam/src/sodam/Driver.java
+++ sodam/src/sodam/Driver.java
@@ -3,6 +3,6 @@ package sodam;
 public class Driver {
     public static void main(String[] args) {
         Menu menu = new Menu();
-        menu.setVisible();
+        menu.setVisible(true);
     }
 }
--- sodam/src/sodam/Menu.java
+++ sodam/src/sodam/Menu.java
@@ -1,84 +1,57 @@
 package sodam;
 
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.awt.image.BufferedImage;
 
 public class Menu extends JFrame {
 
-	//Images
 	private BufferedImage menuImage;
+	private JFrame frame;
 	
-	//Driver constructor
-    public Menu() {
-    	//Read images with required try and catch
+	//Create the application.
+	public Menu() {
     	try {
     		menuImage = ImageIO.read(new File("images/menu.jpg"));
     	} catch (IOException ex) {
     		System.out.println(ex);
     	}
     	
-    	//Create window
-        initUI();
-    }
+		initialize();
+	}
 
-    public final void initUI() {
+	//Initialize the contents of the frame.
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 600, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButton = new JButton("Classic Mode");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frame.getContentPane().add(btnNewButton, BorderLayout.WEST);
+		
+		JButton btnNewButton_1 = new JButton("ULTRA EXTREME MODE");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frame.getContentPane().add(btnNewButton_1, BorderLayout.EAST);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(menuImage));
+		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+	}
 
-       //Creates new panel
-       JPanel panel = new JPanel();
-       getContentPane().add(panel);
-
-       //Allows to position buttons freely
-       panel.setLayout(null);
-
-       //Classic Mode Button
-       JButton classicMode = new JButton("Classic Mode");
-       classicMode.setBounds(50, 60, 200, 30);
-       classicMode.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent event) {
-               System.exit(0);
-          }
-       });
-       
-       JButton quitButton = new JButton("Quit");
-       quitButton.setBounds(50, 60, 80, 30);
-       quitButton.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent event) {
-               System.exit(0);
-          }
-       });
-       
-       //Adds buttons to panel
-       panel.add(quitButton);
-       panel.add(classicMode);
-
-       //Add an image
-       JLabel picLabel = new JLabel(new ImageIcon(menuImage));
-       add(picLabel);
-       
-       //Creates game window
-       setTitle("Sodam Drunken Darts");
-       setSize(600, 600);
-       setLocationRelativeTo(null);
-       setDefaultCloseOperation(EXIT_ON_CLOSE);
-       setResizable(false);
-    }
-    
-    public void setVisible() {
-    	this.setVisible(true);
-    }
-    
-    public void setInvisible() {
-    	this.setVisible(false);
-    }
 }
