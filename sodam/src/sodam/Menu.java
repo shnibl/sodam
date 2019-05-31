814a55ad7a5b4103d2cc21c4652bdc3aeeb104b2
Shnibl <cadester5@gmail.com>
2019-05-31 08:44:41 -0700

    upload

--- sodam/src/sodam/Menu.java
+++ sodam/src/sodam/Menu.java
@@ -11,6 +11,18 @@ import javax.swing.JLabel;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Panel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Menu extends JFrame {
 
@@ -19,12 +31,68 @@ public class Menu extends JFrame {
 	
 	//Create the application.
 	public Menu() {
-    	try {
-    		menuImage = ImageIO.read(new File("images/menu.jpg"));
-    	} catch (IOException ex) {
-    		System.out.println(ex);
-    	}
-    	
+		
+		JPanel panel = new JPanel();
+		panel.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				panel.setLocation(panel.getX() - 1, panel.getY());
+				int keyCode = e.getKeyCode();
+				
+				if (keyCode == KeyEvent.VK_NUMPAD0) {
+					panel.setLocation(panel.getX() - 1, panel.getY());
+				}
+				
+				if (keyCode == KeyEvent.VK_RIGHT) {
+					panel.setLocation(panel.getX() + 1, panel.getY());
+				}
+
+				if (keyCode == KeyEvent.VK_UP) {
+					panel.setLocation(panel.getX(), panel.getY() + 1);
+				}
+
+				if (keyCode == KeyEvent.VK_DOWN) {
+					panel.setLocation(panel.getX(), panel.getY() - 1);
+				}
+			}
+		});
+		panel.setBackground(Color.PINK);
+		
+		//Creates the dart board image
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\tierneyc4262\\Desktop\\sodam\\sodam\\Images\\menu.jpg"));
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(80, Short.MAX_VALUE)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 439, GroupLayout.PREFERRED_SIZE)
+					.addGap(65))
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(222)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(233, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 407, GroupLayout.PREFERRED_SIZE)
+					.addGap(32)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(41, Short.MAX_VALUE))
+		);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 423, Short.MAX_VALUE)
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 143, Short.MAX_VALUE)
+		);
+		//Initializes all the panels, frames, etc.
+		panel.setLayout(gl_panel);
+		getContentPane().setLayout(groupLayout);
 		initialize();
 	}
 
@@ -32,26 +100,9 @@ public class Menu extends JFrame {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 600, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JButton btnNewButton = new JButton("Classic Mode");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		frame.getContentPane().add(btnNewButton, BorderLayout.WEST);
-		
-		JButton btnNewButton_1 = new JButton("ULTRA EXTREME MODE");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		frame.getContentPane().add(btnNewButton_1, BorderLayout.EAST);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(menuImage));
-		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		frame.setVisible(false);
+		frame.setSize(600, 600);
 	}
 
 }
