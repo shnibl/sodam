716bf9f50d0a432a27673f3ddb96a2e47d6676f9
Dylan Lee <LeeD9502@sduhsd.lan>
2019-06-03 11:19:06 -0700

    Dart

--- sodam/src/sodam/Dart.java
+++ sodam/src/sodam/Dart.java
@@ -11,24 +11,30 @@ public class Dart {
 	private double xMid;
 	private double yMid;
 	private double width;
+	private int numRings;
+	private double ringWidth;
 	
 	private int scoreArea;
 	
-	public Dart() {
-		xMid = frame.getXMid();
-		yMid = frame.getYMid();
-		width = frame.getWidth();
+	public Dart(double x, double y) {
+		xMid = 190; //frame.getXMid();
+		yMid = 184; //frame.getYMid();
+		width = 300; //frame.getWidth();
+		numRings = 5; //frame.getNumRings();
+		ringWidth = (width / 2) / (numRings + 1);
 		//Initialize x and y from the cursor's position
-		
+		x1 = x;
+		y1 = y;
 		//Initialize charge as a multiple between 0 and 2 depending on the charge bar
-		
+		charge = 1;
 		//Multiply x1 and x1 by the charge to get x2 and y2
-		
+		x2 = x1;
+		y2 = y1 * charge;
 		//Get the distance from the middle
 		distanceFromMid = calcDistance(xMid, yMid, x2, y2);
 		//Check to see how many points the player gets from the dart throw
 		scoreArea = areaLanded();
-		
+		System.out.println("You landed in " + scoreArea);
 	}
 	
 	public static double calcDistance(double x1, double y1, double x2, double y2) {
@@ -36,17 +42,17 @@ public class Dart {
 	}
 	
 	public int areaLanded() {
-		if (distanceFromMid < 25) {
+		if (distanceFromMid < ringWidth) {
 			return 0;
-		} else if (distanceFromMid < 50) {
+		} else if (distanceFromMid < ringWidth * 2) {
 			return 1;
-		} else if (distanceFromMid < 75) {
+		} else if (distanceFromMid < ringWidth * 3) {
 			return 2;
-		} else if (distanceFromMid < 100) {
+		} else if (distanceFromMid < ringWidth * 4) {
 			return 3;
-		} else if (distanceFromMid < 125) {
+		} else if (distanceFromMid < ringWidth * 5) {
 			return 4;
-		} else if (distanceFromMid < 150) {
+		} else if (distanceFromMid < ringWidth * 6) {
 			return 5;
 		} else {
 			return -1;
