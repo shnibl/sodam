4f523c125700d488aede808b934c94322535b902
Shnibl <cadester5@gmail.com>
2019-06-05 09:38:00 -0700

    calvins howtoplay

--- sodam/src/sodam/HowToPlay.java
+++ sodam/src/sodam/HowToPlay.java
@@ -1,42 +1,46 @@
-package sodam;
-
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class HowToPlay extends JFrame {
 
-	private JFrame frame;
-
-	/**
-	 * Create the application.
-	 */
 	public HowToPlay() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 600, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		
+		JLabel lblSodamDrunkenDarts = new JLabel("SODAM Drunken Darts - How to Play");
+		
+		JLabel lblUseTheArrow = new JLabel("Use the Arrow Keys to move the cursor.");
+		
+		JLabel lblPressAndHold = new JLabel("Press and hold the spacebar to charge your throw.");
+		
+		JLabel lblGameEndsAfter = new JLabel("Game ends after each player has thrown 10 darts.");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 584, Short.MAX_VALUE)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblSodamDrunkenDarts)
+						.addComponent(lblUseTheArrow)
+						.addComponent(lblPressAndHold)
+						.addComponent(lblGameEndsAfter))
+					.addContainerGap(329, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 561, Short.MAX_VALUE)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblSodamDrunkenDarts)
+					.addGap(18)
+					.addComponent(lblUseTheArrow)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblPressAndHold)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblGameEndsAfter)
+					.addContainerGap(464, Short.MAX_VALUE))
 		);
-		frame.getContentPane().setLayout(groupLayout);
+		getContentPane().setLayout(groupLayout);
 	}
 }
