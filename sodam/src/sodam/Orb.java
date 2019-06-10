58fccb7cd6cf711f7fd2686f25572d8333aa207f
Shnibl <cadester5@gmail.com>
2019-06-10 08:51:04 -0700

    Revert "removes stuff from orb"
    This reverts commit 08c94db9ca0a92bc49195a9469e86b29b0c909f8.

--- sodam/src/sodam/Orb.java
+++ sodam/src/sodam/Orb.java
@@ -7,7 +7,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.Graphics;
 
-public class Orb extends JPanel {
+public class Orb extends JPanel implements KeyListener {
 	
 	private int x, velY, height, y, yMid;
 	
@@ -15,6 +15,7 @@ public class Orb extends JPanel {
 		setSize(new Dimension(100, 100));
 		setPreferredSize(new Dimension(100, 100));
 		setFocusable(false);
+		addKeyListener(this);
 		setLayout(null);
 		
 		x = 0;
@@ -48,6 +49,21 @@ public class Orb extends JPanel {
 		if (y <= 0 || y >= getHeight() - height) {
 			velY = -velY;
 		}
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
 	}
 	
 	public int getRectX() {
